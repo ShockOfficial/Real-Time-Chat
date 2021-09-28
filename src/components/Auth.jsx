43d6f31c3d3d42396940bf5cf47e76d0f327a5e4
Paweł <pglmotyka@gmail.com>
2021-09-28 19:28:01 +0200

    change url

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -32,7 +32,8 @@ const Auth = () => {
 
 		const { username, password, phoneNumber, avatarURL } = form;
 
-		const URL = 'http://localhost:5000/auth';
+		// const URL = 'http://localhost:5000/auth';
+		const URL = 'https://shock-chat-app.herokuapp.com/auth';
 
 		const {
 			data: { token, userId, hashedPassword, fullName },
